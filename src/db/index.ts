1cff0c422e66d19cc5b9841e429bdcc74c4f6c8c
Aldrich Santiago <aldrichsantiago20@gmail.com>
2023-12-05 07:47:24 +0800

    modified mysql2 connection options

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,7 +8,12 @@ const poolConnection = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 4,
+  maxIdle: 4, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 30000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
 });
  
 
